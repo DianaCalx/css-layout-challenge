63671b2aed203bce1ac40c4b646d257c7558af8d
Diana Calderon <dianacalderon@ravn.co>
2022-08-11 15:16:11 -0600

    Create data

---
+++ src/data/index.ts
@@ -0,0 +1,66 @@
+export const fileTypes = ['Home', 'My Files', 'Recent Files', 'Shared Filed', 'File Request', 'Trash'];
+
+export const actionFile = ['Upload files', 'Upload folder', 'New folder', 'More'];
+
+export const recently = [
+  {
+    name: 'Add Project',
+    createdAt: '20-02-2020',
+    usersCount: 2
+  },
+  {
+    name: 'Project: fitbit',
+    createdAt: '28-02-2020',
+    usersCount: 2
+  },
+  {
+    name: 'Client Documents',
+    createdAt: '04-03-2020',
+    usersCount: 5
+  }
+];
+
+export const recentFiles = [ 
+  {
+    name: 'Travel Landing Page',
+    members: '5 members',
+    lastModified: 'Mar 8,2020',
+    color: '#FF9F00'
+  },
+  {
+    name: 'True Photos',
+    members: '12 members',
+    lastModified: 'Mar 8,2020',
+    color: '#4AC29D'
+  },
+  {
+    name: 'Dashboard Structure',
+    members: '10 members',
+    lastModified: 'Mar 9,2020',
+    color: "#FF6860"
+  },
+  {
+    name: 'Character Illustration',
+    members: '3 members',
+    lastModified: 'Mar 10,2020',
+    color: '#FF9F00'
+  }
+]
+
+export const shareFiles = [
+  { 
+    name: 'Landing Page',
+    created: '20.02.2020',
+    members: 2
+  },
+  { 
+    name: 'Illustration Pack',
+    created: '20.02.2020',
+    members: 3
+  },
+  { 
+    name: 'CV Design',
+    created: '20.02.2020',
+    members: 2
+  }
+]
