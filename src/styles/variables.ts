3cbc1deb85a726065aced7e68f4b0429d444f4ba
Diana Calderon <dianacalderon@ravn.co>
2022-08-11 15:15:12 -0600

    Update color variables

--- src/styles/variables.ts
+++ src/styles/variables.ts
@@ -1,5 +1,13 @@
 const variables = {
-  backgroundColor: '#E5E5E5',
+  backgroundColor: '#F5F6FC',
+  darkBlueColor: '#343951',
+  darkGrayColor: '#464C61',
+  darkGrayColor2: '#858A9D',
+  lightGrayColor: '#9D9FAF',
+  lightGrayColor2 : '#BCBECA',
+  yellowColor: '#FF9F00',
+  lightYellowColor: '#FFB233',
+  lightYellowColor2: '#FFD999',
 }
 
 export default variables;
